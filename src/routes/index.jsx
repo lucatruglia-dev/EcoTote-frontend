f5db640a526be3a0ae60bf37446fd84af5915c43
Luca Truglia <info.truglialuca@gmail.com>
2025-05-11 10:57:30 +0200

    Aggiunta logout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,25 @@
 import { Title } from "@solidjs/meta";
+import { createEffect } from "solid-js";
 import './main.css';
 
 export default function Home() {
+  createEffect(() => {
+    // Make POST request when component mounts
+    fetch("https://api.ecotote.it/api/v1/auth/logout", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "include"
+    }).catch(err => console.error("Error logging access:", err));
+  });
+
   return (
     <>
       <header>
-        <img src="../assets/images/corn.svg" className="corn" />
-        <img src="../assets/images/home_logo.png" className="logo" />
+        <img src="assets/images/corn.svg" className="corn" />
+        <img src="assets/images/home_logo.png" className="logo" />
       </header>
 
       <div className="content">
